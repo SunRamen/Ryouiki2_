e03cfb177b0881203c4338cb22b966ebff860971
SunRamen <epd67755@kwansei.ac.jp>
2023-06-18 23:08:40 +0900

    i

--- r2_1.py
+++ r2_1.py
@@ -1,6 +1,14 @@
 import zipfile
 
+sum = 0
+
 with zipfile.ZipFile('sample.zip','r') as zip_data:
-    for txt in zip_data:
-        i = zip_data.read()
-        print(i)
+    for name in zip_data.namelist():
+        if "kitamura" in name:
+            if int(name.split('_')[1]) % 2 != 0:
+                with zip_data.open(name, 'r') as file:
+                    txt = file.read()
+                    num = int(txt.strip())
+                    sum += num
+
+print("奇数ファイルの合計",sum)
